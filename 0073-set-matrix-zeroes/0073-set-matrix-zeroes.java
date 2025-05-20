89901fc719d7db16bd3c112bdbee9e17b10e2bff
MahfoozKhan333 <mahfoozkhan0333@gmail.com>
2025-05-20 09:38:50 +0530

    Time: 1 ms (72.87%), Space: 46.2 MB (8.35%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -19,11 +19,11 @@ class Solution {
                     matrix[i][j]=0;
 
         if(firstRow)
-            for(int j=0; j<matrix[0].length;j++)
+            for(int j=1; j<matrix[0].length;j++)
                  matrix[0][j]=0;
 
         if(firstCol)
-            for(int i=0; i<matrix.length;i++)
+            for(int i=1; i<matrix.length;i++)
                 matrix[i][0]=0;
         
     }
