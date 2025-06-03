2b0fa80cdad71725649319ed2f2672657ec964b2
MahfoozKhan333 <mahfoozkhan0333@gmail.com>
2025-06-03 08:04:32 +0530

    Time: 24 ms (6.01%), Space: 14 MB (14.75%) - LeetHub

---
+++ 0889-buddy-strings/0889-buddy-strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        if len(s) != len(goal):
+            return False
+
+        if s == goal:
+        
+            return len(set(s)) < len(s)
+
+        diffs = [(a, b) for a, b in zip(s, goal) if a != b]
+
+        return len(diffs) == 2 and diffs[0] == diffs[1][::-1]
